6960fe9c1b19f7edb4e88ecb9284dff3e8b388fa
dimahalaiskiy <dgalaiskiy@icloud.com>
2025-07-23 10:28:08 +0300

    CHANGED: ApolloProvider destination

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "@/styles/globals.css";
-import ApolloProvider from "./ApolloProvider";
+import ApolloProvider from "@/providers/ApolloProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
